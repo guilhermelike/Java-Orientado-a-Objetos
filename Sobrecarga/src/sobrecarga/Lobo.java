27d8cfd9a7849ae9732bad913490a32274b5c293
Guilherme <101355723+guilhermelike@users.noreply.github.com>
2023-07-29 14:44:16 -0300

    lesson 13

---
+++ Sobrecarga/src/sobrecarga/Lobo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sobrecarga;
+
+/**
+ *
+ * @author gfbat
+ */
+public class Lobo extends Mamifero{
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuu!");
+    }
+    
+}
---
+++ Sobrecarga/src/sobrecarga/Sobrecarga.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sobrecarga;
+
+/**
+ *
+ * @author gfbat
+ */
+public class Sobrecarga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mamifero m = new Mamifero();
+        Lobo l = new Lobo();
+        Cachorro c = new Cachorro();
+        
+        m.emitirSom();
+        l.emitirSom();
+        c.emitirSom();
+        
+        c.reagir("Olá");
+        c.reagir("Apanhar");
+        c.reagir(11, 45);
+        c.reagir(21, 00);
+        c.reagir(true);
+        c.reagir(false);
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
+    }
+    
+}
